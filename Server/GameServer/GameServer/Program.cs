35698a573740b24d4c3ff2f961501fa59a821857
Hojin Kim <hojiny9923@gmail.com>
2021-09-06 16:37:34 +0900

    Init
    언젠간 하겟죠... 주인님?

---
+++ Server/GameServer/GameServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("일단 cmd창만 올리는걸로");
+            Console.WriteLine("소켓이고 뭐고 아직 뭔소린지 1도 모르겟슴");
+
+            //loop
+            while (true)
+            {
+
+            }
+        }
+    }
+}
