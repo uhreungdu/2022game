9514d13065136ca885098c2b43e387f66bceccf6
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2021-12-25 21:56:20 +0900

    PlayerBasicAttack
    공격에 딜레이를 만듬

--- MachineBuildingDestroy/Assets/Scripts/PlayerBasicAttack.cs
+++ MachineBuildingDestroy/Assets/Scripts/PlayerBasicAttack.cs
@@ -8,6 +8,10 @@ public class PlayerBasicAttack : MonoBehaviour
     public PlayerInput playerInput;
     public BoxCollider boxCollider;
 
+    public float timeBetAttack = 0.5f; // 공격 간격
+    public float  activeAttackTime = 0.3f; // 공격 유지 시간
+    private float lastAttackTime; // 공격을 마지막에 한 시점
+
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -19,20 +23,30 @@ public class PlayerBasicAttack : MonoBehaviour
     {
         if (playerInput.fire)
         {
-            Debug.Log("공격");
-            boxCollider.enabled = true;
+            if (Time.time >= lastAttackTime + timeBetAttack)
+            {
+                if (Time.time >= lastAttackTime + timeBetAttack + activeAttackTime)
+                {
+                    lastAttackTime = Time.time;
+                }
+                boxCollider.enabled = true;
+            }
+            else
+            {
+                // 코드 너무 이상하게 짠듯 나중에 바꿈
+                boxCollider.enabled = false;
+            }
         }
         else
         {
-            Debug.Log("공격 안나감");
             boxCollider.enabled = false;
         }
     }
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행
         Wall attackTarget = other.GetComponent<Wall>();
-        if (attackTarget != null)
+        if (attackTarget != null && !attackTarget.dead)
         {
             Debug.Log(attackTarget.hp);
             attackTarget.OnDamage(20);
--- MachineBuildingDestroy/Assets/Scripts/Wall.cs
+++ MachineBuildingDestroy/Assets/Scripts/Wall.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     public float hp = 100f;
+    public bool dead { get; protected set; } // 사망 상태
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +15,23 @@ public class Wall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public void OnDamage(float damage)
     {
         hp -= damage;
+
+        // 체력이 0 이하 && 아직 죽지 않았다면 사망 처리 실행
+        if (hp <= 0 && !dead)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        // 사망 상태를 참으로 변경
+        dead = true;
     }
 }
