da6d57bc93428d4c0e835151b3af7836a680792f
Hojin Kim <hojiny9923@gmail.com>
2022-01-06 02:37:02 +0900

    transform 갱신 최적화

--- MachineBuildingDestroy/Assets/Scripts/RenewCamTransform.cs
+++ MachineBuildingDestroy/Assets/Scripts/RenewCamTransform.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class RenewCamTransform : MonoBehaviour
 {
+    Transform tr;
     // Start is called before the first frame update
     void Start()
     {
         this.gameObject.transform.SetPositionAndRotation(GameObject.FindWithTag("MainCamera").GetComponent<Transform>().position, GameObject.FindWithTag("MainCamera").GetComponent<Transform>().rotation);
-        GameObject.FindWithTag("MainCamera").GetComponent<Transform>();
+        tr = GameObject.FindWithTag("MainCamera").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.SetPositionAndRotation(GameObject.FindWithTag("MainCamera").GetComponent<Transform>().position, GameObject.FindWithTag("MainCamera").GetComponent<Transform>().rotation);
+        this.gameObject.transform.SetPositionAndRotation(tr.position, tr.rotation);
     }
 }
