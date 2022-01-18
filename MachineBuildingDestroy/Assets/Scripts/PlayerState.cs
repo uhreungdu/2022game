c25dfb55c822d84219724e7d68d887a46092303f
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-01-18 17:27:53 +0900

    팀 나누기
    조금더 유기적으로 나누는 방법을 강구해봐야 할듯

--- MachineBuildingDestroy/Assets/Scripts/PlayerState.cs
+++ MachineBuildingDestroy/Assets/Scripts/PlayerState.cs
@@ -2,12 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerState : MonoBehaviour
+public class PlayerState : LivingEntity
 {
+    public int team { get; private set; }
     // Start is called before the first frame update
+    public AudioClip deathClip; // 사망 소리
+    public AudioClip hitClip; // 피격 소리
+
+    private AudioSource playerAudioPlayer; // 플레이어 소리 재생기
+    private Animator playerAnimator; // 플레이어의 애니메이터
+
     void Start()
     {
-        
+        playerAnimator = GetComponent<Animator>();          // 지금 안됨
+        playerAudioPlayer = GetComponent<AudioSource>();    // 지금 안됨
+        team = 1;
+    }
+    protected override void OnEnable()
+    {
+        // LivingEntity의 OnEnable() 실행 (상태 초기화)
+        base.OnEnable();
+        team = Random.Range(0, 1);
     }
 
     // Update is called once per frame
