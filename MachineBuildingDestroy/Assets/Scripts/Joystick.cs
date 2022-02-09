616382529fc1e556b1efe1aebc2da43d3ddca166
Hojin Kim <hojiny9923@gmail.com>
2022-02-10 02:16:24 +0900

    Joystick works
    안드로이드 환경에서 작동 확인
    
    문제점)
    * 조이스틱 조작이 화면 시점 회전에 영향을 줌
    * 조이스틱 조작 거리와 상관없이 최고속도로 이동함

--- MachineBuildingDestroy/Assets/Scripts/Joystick.cs
+++ MachineBuildingDestroy/Assets/Scripts/Joystick.cs
@@ -12,6 +12,9 @@ public class Joystick : MonoBehaviour, IBeginDragHandler, IEndDragHandler,IDragH
     [SerializeField, Range(10f, 200f)]
     private float moveRange;
 
+    public Vector2 moveVector;
+    private bool IsInput;
+
     private void Awake()
     {
         joystickBack = GetComponent<RectTransform>();
@@ -21,37 +24,40 @@ public class Joystick : MonoBehaviour, IBeginDragHandler, IEndDragHandler,IDragH
     {
         //Debug.Log("BeginDrag");
 
-        var inputDir = eventData.position - joystickBack.anchoredPosition;
-        var clampedDir = inputDir;
-        if(inputDir.magnitude > moveRange)
-        {
-            clampedDir = inputDir.normalized * moveRange;
-        }
-        else
-        {
-            clampedDir = inputDir;
-        }
-        joystick.anchoredPosition = clampedDir;
+        MoveJoystick(eventData);
+        IsInput = true;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         //Debug.Log("OnDrag");
-        var inputDir = eventData.position - joystickBack.anchoredPosition;
-        var clampedDir = inputDir;
-        if (inputDir.magnitude > moveRange)
-        {
-            clampedDir = inputDir.normalized * moveRange;
-        }
-        else
-        {
-            clampedDir = inputDir;
-        }
-        joystick.anchoredPosition = clampedDir;
+        MoveJoystick(eventData);    
     }
     public void OnEndDrag(PointerEventData eventData)
     {
         //Debug.Log("EndDrag");
         joystick.anchoredPosition = Vector2.zero;
+        moveVector = Vector2.zero;
+        IsInput = false;
+    }
+
+    void Update()
+    {
+        if (IsInput)
+        {
+            Debug.Log(moveVector);
+        }
+    }
+
+    public void MoveJoystick(PointerEventData eventData)
+    {
+        var inputDir = eventData.position - joystickBack.anchoredPosition;
+        var clampedDir = inputDir;
+        if (inputDir.magnitude > moveRange)
+            clampedDir = inputDir.normalized * moveRange;
+        else
+            clampedDir = inputDir;
+        joystick.anchoredPosition = clampedDir;
+        moveVector = clampedDir / moveRange;
     }
 }
--- MachineBuildingDestroy/Assets/Scripts/PlayerInput.cs
+++ MachineBuildingDestroy/Assets/Scripts/PlayerInput.cs
@@ -10,6 +10,15 @@ public class PlayerInput : MonoBehaviour
     public string JumpButtonName = "Jump"; // 발사를 위한 입력 버튼 이름
     public string DashButtonName = "Dash"; // 발사를 위한 입력 버튼 이름
 
+    private Joystick joystick;
+
+    void Start()
+    {
+        // 에디터 상에서 체크할려면 WindowsEditor로 해야됨
+        if (Application.platform == RuntimePlatform.Android)
+            joystick = GameObject.Find("Joystickback").GetComponent<Joystick>();
+    }
+
     // 값 할당은 내부에서만 가능
     public float move { get; private set; } // 감지된 움직임 입력값
     public float rotate { get; private set; } // 감지된 회전 입력값
@@ -31,6 +40,11 @@ public class PlayerInput : MonoBehaviour
         move = Input.GetAxis(moveAxisName);
         // rotate에 관한 입력 감지
         rotate = Input.GetAxis(rotateAxisName);
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            move = joystick.moveVector.y;
+            rotate = joystick.moveVector.x;
+        }
         // fire에 관한 입력 감지
         fire = Input.GetButton(fireButtonName);
 
