75b4c4a6bae2b23e85df933c492233ff8f08083a
2017182026 / youn jang hyurk <34494633+uhreungdu@users.noreply.github.com>
2022-02-15 18:02:52 +0900

    item상자 제작 및 disslove효과 추가

--- MachineBuildingDestroy/Assets/Scripts/Item_Beacon_Control.cs
+++ MachineBuildingDestroy/Assets/Scripts/Item_Beacon_Control.cs
@@ -7,18 +7,41 @@ public class Item_Beacon_Control : MonoBehaviour
     // Start is called before the first frame update
     public GameManager Gmanager;
     public int min;
+    public int past_min;
     public float sec;
     public GameObject box;
+    public GameObject box_obj;
     public bool have_box;
     void Start()
     {
         Gmanager = GameManager.GetInstance();
-        box.GetComponent<item_box_make>().decide_type();
+        box = Resources.Load<GameObject>("item_box");
+        box_obj = Instantiate(box);
+        box_obj.transform.SetParent(gameObject.transform);
+        box_obj.transform.Translate(gameObject.transform.position);
+        box_obj.GetComponent<item_box_make>().decide_type();
+        //box_obj.GetComponent<item_box_make>().effect_On = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        box.GetComponent<item_box_make>().effect_On = true;
+        if(box_obj != null)
+        {
+            if(Gmanager.now_timer.sec >= 30)
+            {
+                box_obj.GetComponent<item_box_make>().effect_On = true;
+            }
+            
+        }
+        else
+        {
+            box = Resources.Load<GameObject>("item_box");
+            box_obj = Instantiate(box);
+            box_obj.transform.SetParent(gameObject.transform);
+            box_obj.transform.Translate(gameObject.transform.position);
+            box_obj.GetComponent<item_box_make>().decide_type();
+
+        }
     }
 }
--- MachineBuildingDestroy/Assets/Scripts/item_box_make.cs
+++ MachineBuildingDestroy/Assets/Scripts/item_box_make.cs
@@ -12,9 +12,19 @@ public class item_box_make : MonoBehaviour
     public item_type now_type;
     public bool effect_On = false;
     // Start is called before the first frame update
+    [SerializeField] private float noiseStrength = 0.25f;
+    [SerializeField] private float objectHeight = 1.0f;
+    public float Time_temp;
+    public bool effect_switch;
+    public float move_height;
+    private Material material;
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
     void Start()
     {
-        effect_On = false;
+        effect_switch = true;
     }
 
     // Update is called once per frame
@@ -22,7 +32,21 @@ public class item_box_make : MonoBehaviour
     {
         if(effect_On == true)
         {
-            //decide_type();
+            if(move_height <= 2)
+            {
+                if(Mathf.Sin(Time_temp) >= 0.99f)
+                {
+                    move_height += 2;
+                    effect_switch = true;
+                }
+                else
+                {
+                    Time_temp += Time.deltaTime * Mathf.PI * 0.1f;
+                    move_height += Mathf.Sin(Time_temp)* 2;
+                    SetHeight(move_height);
+                    move_height = 0;
+                }
+            }
             transform.Rotate(Vector3.up * 20 * Time.deltaTime);
         }
     }
@@ -47,6 +71,11 @@ public class item_box_make : MonoBehaviour
         if(col.tag == "Player"){
             Destroy(gameObject);
         }
+    }
+    public void SetHeight(float height)
+    {
+        material.SetFloat("CutoffHeight",height);
+        material.SetFloat("Nosie_strength",noiseStrength);
     }
 
 }
