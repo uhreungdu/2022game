60d23cc2bce27855fadd1e538329ac2ee2fa80dd
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-02-21 16:54:03 +0900

    맵의 자식으로 넘어가게 수정

--- MachineBuildingDestroy/Assets/Scripts/Map.cs
+++ MachineBuildingDestroy/Assets/Scripts/Map.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    private List<GameObject> walls = new List<GameObject>(); // 생성된 적들을 담는 리스트
+    private List<GameObject> Buildings = new List<GameObject>(); // 생성된 적들을 담는 리스트
 
     public GameObject planepref;
     public GameObject bulidingpref;
@@ -22,25 +22,32 @@ public class Map : MonoBehaviour
         //{
 
         //}
-        //offset = plane.GetComponent<RectTransform>().rect.width;
         float offset = 10;
-        Vector3 position = Vector3.zero;
-        GameObject temp = Instantiate(planepref, position, planepref.transform.rotation);
-        temp.transform.localScale += new Vector3(x - 1, 0, z - 1);
+        for (int i = 0; i < x; ++i)
+        {
+            for (int j = 0; j < z; ++j)
+            {
+                Vector3 position = new Vector3((i * offset) - (offset * ((x - 1) / 2)), 0, (j * offset) - (offset * ((z - 1) / 2)));
+                GameObject temp = Instantiate(planepref, position, planepref.transform.rotation);
+                temp.transform.SetParent(this.transform);
+            }
+        }
         for (int i = 0; i < 100; ++i)
         {
-                position.x = Random.RandomRange(-offset * (x / 2) + offset, offset * (x / 2) - offset);
-                position.y = Random.RandomRange(20, 50);
+            Vector3 position = Vector3.zero;
+            position.x = Random.RandomRange(-offset * (x / 2) + offset, offset * (x / 2) - offset);
+            position.y = Random.RandomRange(20, 50);
             position.z = Random.RandomRange(-offset * (z / 2) + offset, offset * (z / 2) - offset);
 
-            GameObject wall = Instantiate(bulidingpref, position, bulidingpref.transform.rotation);
-            walls.Add(wall);
+            GameObject buliding = Instantiate(bulidingpref, position, bulidingpref.transform.rotation);
+            Buildings.Add(buliding);
+            buliding.transform.SetParent(this.transform);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
