02d007c9a21c03db029a1d7ead4d79b4e19ec5c8
Hojin Kim <32784096+HOTOMARI@users.noreply.github.com>
2022-02-23 22:05:28 +0900

    Update add_room.php
    방 생성 시 primary key 중복에 의한 상황 처리
    방 생성 player를 playingchar table에 추가

--- WebDBServer/add_room.php
+++ WebDBServer/add_room.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysqli_connect("localhost","root","password","test");
+
+$conn = mysqli_connect("localhost","root","hj7856","test");
 
 $iname=$_GET['iname'];
 $ename=$_GET['ename'];
@@ -7,8 +8,17 @@ $nowPnum=$_GET['nowPnum'];
 $maxPnum=$_GET['maxPnum'];
 $Pname=$_GET['Pname'];
 
+// 이미 생성된 방 체크    
+$result = mysqli_query($conn,"SELECT COUNT(*) FROM room WHERE internal_name=$iname;");
+$resultval = $result->fetch_array()[0];
+if($resultval > 0){
+    echo('이미 생성된 방');
+    exit();
+}
+
+// 방 생성
 mysqli_query($conn,"
-INSERT INTO room (
+INSERT IGNORE INTO room (
     internal_name,
     external_name,
     now_playernum,
@@ -22,4 +32,21 @@ INSERT INTO room (
         $maxPnum,
         $Pname
         );
-        ");
+        "
+    );
+
+// Player join room 
+mysqli_query($conn,"
+INSERT IGNORE INTO playingchar (
+    character_name,
+    room_internal_name
+    ) 
+    VALUE (
+        $Pname,
+        $iname
+        );
+        "
+    );
+echo ('방 생성 성공');
+
+?>
