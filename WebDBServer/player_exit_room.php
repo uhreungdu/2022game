fec1a4a4c3ac0543241577cd754ec5ce3061f6d8
Hojin Kim <32784096+HOTOMARI@users.noreply.github.com>
2022-02-24 20:20:22 +0900

    방 나가기 방 제거

---
+++ WebDBServer/player_exit_room.php
@@ -0,0 +1,33 @@
+<?php
+
+$conn = mysqli_connect("localhost","root","hj7856","test");
+
+$iname=$_GET['iname'];
+$Pname=$_GET['Pname'];
+
+// 플레이중인 유저 목록에서 제거
+mysqli_query($conn,"
+DELETE FROM playingchar
+WHERE character_name = '$Pname';
+");
+
+// 방 현재 인원수 감소
+mysqli_query($conn,"
+UPDATE room
+SET now_playernum = now_playernum - 1
+WHERE internal_name = '$iname';
+");
+
+// 방 인원수가 0명 이하이면 방 제거
+$result = mysqli_query($conn,"
+SELECT now_playernum FROM room 
+WHERE internal_name = '$iname';
+");
+$resultval = $result->fetch_array()[0];
+if($resultval <= 0){
+    echo('인원수 0명 이하 방 제거');
+    mysqli_query($conn,"
+    DELETE FROM room
+    WHERE internal_name = '$iname';
+    ");
+}
