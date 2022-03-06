b1854c010470ba25402bf1eb087ccd50db1be9f8
Hojin Kim <hojiny9923@gmail.com>
2022-03-06 17:24:11 +0900

    로그인 요청
    그 후 처리 작성해야 됨

--- MachineBuildingDestroy/Assets/Scripts/login_test/LoginButton.cs
+++ MachineBuildingDestroy/Assets/Scripts/login_test/LoginButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class LoginButton : MonoBehaviour
 {
@@ -23,8 +24,41 @@ public class LoginButton : MonoBehaviour
 
     public void OnClick()
     {
+        // 로그인 창 비활성화
         GetComponent<Button>().interactable = false;
         IDInput.GetComponent<InputField>().interactable = false;
         PWInput.GetComponent<InputField>().interactable = false;
+        
+        // 로그인 요청
+        StartCoroutine(LoginRequest());
+    }
+    
+    IEnumerator LoginRequest()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("id", "\""+IDInput.GetComponent<InputField>().text+"\"") ;
+        form.AddField("pw", "\"" + PWInput.GetComponent<InputField>().text + "\"");
+
+        UnityWebRequest www = UnityWebRequest.Post("http://121.139.87.70/login_account.php", form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+            // 로그인 창 활성화
+            GetComponent<Button>().interactable = true;
+            IDInput.GetComponent<InputField>().interactable = true;
+            PWInput.GetComponent<InputField>().interactable = true;
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+            string results = www.downloadHandler.text;
+            Debug.Log(results);
+            // 로그인 창 활성화
+            GetComponent<Button>().interactable = true;
+            IDInput.GetComponent<InputField>().interactable = true;
+            PWInput.GetComponent<InputField>().interactable = true;
+        }
     }
 }
