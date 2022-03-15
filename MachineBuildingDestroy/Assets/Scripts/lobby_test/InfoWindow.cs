b6997caee4597368ae9bb1400c2308ebd1f386b6
Hojin Kim <hojiny9923@gmail.com>
2022-03-16 01:49:24 +0900

    정보 창 나올 때 애니메이션
    값 가져오는 시간 감출려고 해봤는데 값 가져오는게 넘모 느림
    나중에 더 잘 숨기게 바꿔봐야될듯

--- MachineBuildingDestroy/Assets/Scripts/lobby_test/InfoWindow.cs
+++ MachineBuildingDestroy/Assets/Scripts/lobby_test/InfoWindow.cs
@@ -20,7 +20,23 @@ public class InfoWindow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!this.gameObject.activeSelf) return;
+        if (this.transform.localPosition.y > 0)
+        {
+            this.transform.Translate(0, -1600.0f * Time.deltaTime, 0);
+        }
+        else if(this.transform.localScale.x<=0.4166667f)
+        {
+            this.transform.localScale += new Vector3(0.4166667f * Time.deltaTime * 4.0f, 0, 0);
+        }
+    }
+
+    private void OnDisable()
+    {
+        Vector3 initPos = new Vector3(0, 400.0f, 0);
+        Vector3 initSca = new Vector3(0, 0.4166667f, 0.4166667f);
+        this.transform.localPosition = initPos;
+        this.transform.localScale = initSca;
     }
 
     private void Awake()
