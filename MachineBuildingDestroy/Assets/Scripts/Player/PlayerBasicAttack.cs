767859525b68ae92a4bbe9568806ec2a94c2c4d3
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-04-04 16:51:32 +0900

    남은거 저장

--- MachineBuildingDestroy/Assets/Scripts/Player/PlayerBasicAttack.cs
+++ MachineBuildingDestroy/Assets/Scripts/Player/PlayerBasicAttack.cs
@@ -96,21 +96,21 @@ public class PlayerBasicAttack : MonoBehaviourPun
         // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행
         if (other.tag == "Wall")
         {
-            WallObject attackTarget = other.GetComponent<WallObject>();
-            if (attackTarget != null && !attackTarget.dead)
-            {
-                Material material = other.GetComponent<MeshRenderer>().sharedMaterial;
-                if (material == null)
-                {
-                    material = other.GetComponentInChildren<MeshRenderer>().sharedMaterial;
-                }
-                // CMeshSlicer.SlicerWorld(other.gameObject, Upvector, other.ClosestPointOnBounds(boxCollider.transform.position), boxmaterial);
-                
-                attackTarget.OnDamage(20);
-                attackTarget.WallDestroy();
-                
-                Debug.Log(attackTarget.health);
-            }
+            // WallObject attackTarget = other.GetComponent<WallObject>();
+            // if (attackTarget != null && !attackTarget.dead)
+            // {
+            //     Material material = other.GetComponent<MeshRenderer>().sharedMaterial;
+            //     if (material == null)
+            //     {
+            //         material = other.GetComponentInChildren<MeshRenderer>().sharedMaterial;
+            //     }
+            //     // CMeshSlicer.SlicerWorld(other.gameObject, Upvector, other.ClosestPointOnBounds(boxCollider.transform.position), boxmaterial);
+            //     
+            //     attackTarget.OnDamage(20);
+            //     attackTarget.WallDestroy();
+            //     
+            //     Debug.Log(attackTarget.health);
+            // }
         }
 
         if (other.tag == "DestroyWall")
