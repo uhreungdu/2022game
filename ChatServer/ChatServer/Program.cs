e3d881b71fceba14a10c4902b4fe1133222f0662
Hojin Kim <hojiny9923@gmail.com>
2022-04-10 21:04:23 +0900

    기본 소켓 틀

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -8,7 +8,23 @@ namespace Chatserver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TTT");
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // PORTNUM is 9888
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9888);
+            server.Bind(ipep);
+
+            // Max waiting is 100;
+            server.Listen(100);
+            Console.WriteLine("Start listen client on PORT 9888...");
+
+            Socket client = server.Accept();
+            Console.WriteLine("A client join server");
+
+            client.Close();
+            Console.WriteLine("A client disconnect server");
+            server.Close();
+            Console.WriteLine("Server end...");
         }
     }
 }
