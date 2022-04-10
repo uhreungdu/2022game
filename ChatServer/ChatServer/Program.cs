1c5daba939af5cb7216f48f39fbdbf255451209b
Hojin Kim <hojiny9923@gmail.com>
2022-04-10 20:45:35 +0900

    프로젝트 생성

---
+++ ChatServer/ChatServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chatserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TTT");
+        }
+    }
+}
