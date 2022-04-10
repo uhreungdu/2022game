5a584a63ac6b294528bd24b25a8c626436d75d64
Hojin Kim <hojiny9923@gmail.com>
2022-04-10 21:40:40 +0900

    메세지 통신 성공
    문장 하나 전송함

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -1,25 +1,33 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Chatserver
 {
     class Program
     {
+        private const int Port = 9888;
+        private const int MaxConnections = 100;
+        private const int BufSize = 128;
         static void Main(string[] args)
         {
             Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            // PORTNUM is 9888
-            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9888);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, Port);
             server.Bind(ipep);
 
-            // Max waiting is 100;
-            server.Listen(100);
+            server.Listen(MaxConnections);
             Console.WriteLine("Start listen client on PORT 9888...");
 
             Socket client = server.Accept();
             Console.WriteLine("A client join server");
+
+            byte[] buffer = new byte[BufSize];
+            int num = client.Receive(buffer);
+
+            string recvdata = Encoding.UTF8.GetString(buffer, 0, num);
+            Console.WriteLine(recvdata);
 
             client.Close();
             Console.WriteLine("A client disconnect server");
--- MachineBuildingDestroy/Assets/Scripts/Chatting/ChatClient.cs
+++ MachineBuildingDestroy/Assets/Scripts/Chatting/ChatClient.cs
@@ -4,23 +4,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 public class ChatClient : MonoBehaviour
 {
     private const string ServerAddress = "127.0.0.1";
     private const int Port = 9888;
-    private TcpClient _client;
-    
+    private const int BufSize = 128;
+    private Socket _client;
+    private IPEndPoint _ipep;
+
+    private void Awake()
+    {
+        _client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        _ipep = new IPEndPoint(IPAddress.Parse(ServerAddress), Port);
+    }
+
     private void Start()
     {
-        try
-        {
-            _client = new TcpClient(ServerAddress, Port);
-        }
-        catch (Exception err)
-        {
-            Debug.Log("Connect ERR");
-        }
+
+            _client.Connect(_ipep);
+
+
     }
-    
+
+    public void SendChat(string msg)
+    {
+        _client.Send(Encoding.UTF8.GetBytes(msg));
+    }
 }
