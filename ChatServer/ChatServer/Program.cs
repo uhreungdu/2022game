ee4e42e561a1a10636e82190e5e65600736d2bc7
Hojin Kim <hojiny9923@gmail.com>
2022-04-13 13:06:54 +0900

    비동기 소켓서버 준비

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -10,17 +10,31 @@ namespace Chatserver
         private const int Port = 9888;
         private const int MaxConnections = 100;
         private const int BufSize = 128;
-        static void Main(string[] args)
+
+        private static Socket? _ServerSocket;
+        private static List<Socket>? _ClientSocketList;
+
+        static void Main()
         {
-            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _ClientSocketList = new List<Socket>();
+            _ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             IPEndPoint ipep = new IPEndPoint(IPAddress.Any, Port);
-            server.Bind(ipep);
+            _ServerSocket.Bind(ipep);
 
-            server.Listen(MaxConnections);
-            Console.WriteLine("Start listen client on PORT 9888...");
+            _ServerSocket.Listen(MaxConnections);
+            Console.WriteLine("Start listen client on PORT " + Port + ".");
 
-            Socket client = server.Accept();
+            while (true)
+            {
+                Console.WriteLine("접속 대기 중");
+                Socket client = _ServerSocket.Accept();
+                Console.WriteLine(client.RemoteEndPoint.ToString() + " 연결");
+                _ClientSocketList.Add(client);
+            }
+
+            /*
+            Socket client = _ServerSocket.Accept();
             Console.WriteLine("A client join server");
 
             byte[] buffer = new byte[BufSize];
@@ -31,8 +45,35 @@ namespace Chatserver
 
             client.Close();
             Console.WriteLine("A client disconnect server");
-            server.Close();
+            _ServerSocket.Close();
             Console.WriteLine("Server end...");
+            */
+        }
+
+        private static void AcceptCallback(object? sender, SocketAsyncEventArgs ev)
+        {
+            Socket? socket = ev.AcceptSocket;
+            if (socket != null)
+            {
+                _ClientSocketList.Add(socket);
+                Console.WriteLine(socket.RemoteEndPoint.ToString()+" 연결");
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                byte[] buffer = new byte[BufSize];
+                args.SetBuffer(buffer, 0, buffer.Length);
+                socket.ReceiveAsync(args);
+            }
+            ev.AcceptSocket = null;
+            _ServerSocket.AcceptAsync(ev);
+        }
+
+        private void ReceiveCallback()
+        {
+
+        }
+
+        private void SendCallback()
+        {
+
         }
     }
 }
