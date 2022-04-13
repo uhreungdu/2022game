8233c0e87715479a7bec9b238e97814cb1c9319c
Hojin Kim <hojiny9923@gmail.com>
2022-04-13 14:05:06 +0900

    메세지 받음
    강제 접속해제에 대한 예외 처리 안됨(해야됨)

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -13,6 +13,9 @@ namespace Chatserver
     }
     class Program
     {
+        // 이벤트로 While문 제어
+        // 접속 받는동안은 무한정으로 돌아갈 이유 없음
+        // 그리고 요거 안걸면 미친듯이 불림
         public static ManualResetEvent allDone = new ManualResetEvent(false);
 
         private const int Port = 9888;
@@ -25,29 +28,32 @@ namespace Chatserver
         static void Main()
         {
             _ClientSocketList = new List<Socket>();
+            // 서버 소켓 생성
             _ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
             IPEndPoint ipep = new IPEndPoint(IPAddress.Any, Port);
-            _ServerSocket.Bind(ipep);
 
-            _ServerSocket.Listen(MaxConnections);
-            Console.WriteLine("Start listen client on PORT " + Port + ".");
-
-            while (true)
+            try
             {
-                allDone.Reset();
-                Console.WriteLine("접속 대기 중");
-                _ServerSocket.BeginAccept(new AsyncCallback(AcceptCallback), _ServerSocket);
-                allDone.WaitOne();
-                //Socket client = _ServerSocket.Accept();
-                
-               // _ClientSocketList.Add(client);
+                _ServerSocket.Bind(ipep);
+                _ServerSocket.Listen(MaxConnections);
+                Console.WriteLine("Start listen client on PORT " + Port + ".");
 
-                //SocketAsyncEventArgs args = new SocketAsyncEventArgs();
-               // byte[] buffer = new byte[BufSize];
-               // args.SetBuffer(buffer);
-               // args.Completed+= new EventHandler<SocketAsyncEventArgs>()
-               // client.ReceiveAsync();
+                while (true)
+                {
+                    // 이벤트 세팅
+                    allDone.Reset();
+
+                    Console.WriteLine("접속 대기 중");
+                    _ServerSocket.BeginAccept(new AsyncCallback(AcceptCallback), _ServerSocket);
+
+                    // 다 될때까지 기다림
+                    allDone.WaitOne();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
             }
 
             /*
@@ -69,23 +75,54 @@ namespace Chatserver
 
         private static void AcceptCallback(IAsyncResult ar)
         {
-            allDone.Set();
-
             Socket server = (Socket)ar.AsyncState;
             Socket client = server.EndAccept(ar);
-            Console.WriteLine(client.RemoteEndPoint.ToString() + " 연결");
+            Console.WriteLine(client.RemoteEndPoint.ToString() + " Connected");
+
+            // 접속 됐다고 알려 줌
+            allDone.Set();
 
             Session session = new Session();
             session.socket = client;
-
+            client.BeginReceive(session.buf, 0, Session.bufSize, 0, 
+                new AsyncCallback(ReceiveCallback), session);
         }
 
-        private void ReceiveCallback()
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            String data = String.Empty;
+
+            Session session = (Session)ar.AsyncState;
+            Socket socket = session.socket;
+
+            try
+            {
+                int recvsize = socket.EndReceive(ar);
+
+                if (recvsize > 0)
+                {
+                    data = Encoding.UTF8.GetString(session.buf, 0, recvsize);
+                    Console.WriteLine(data);
+                }
+                else
+                {
+                    socket.BeginReceive(session.buf, 0, Session.bufSize, 0,
+                        new AsyncCallback(ReceiveCallback), session);
+                }
+            }
+            catch(Exception ex)
+            {
+                socket.Close();
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void SendCallback()
         {
 
         }
 
-        private void SendCallback()
+        private static void Send()
         {
 
         }
