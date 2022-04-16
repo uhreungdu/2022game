12460d6b2607ab0cf68ecab033d704f946372ec9
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-04-16 22:25:06 +0900

    코인 돌아가게 수정

--- MachineBuildingDestroy/Assets/Scripts/cointrigger.cs
+++ MachineBuildingDestroy/Assets/Scripts/cointrigger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class cointrigger : MonoBehaviour
@@ -7,6 +8,7 @@ public class cointrigger : MonoBehaviour
     // Start is called before the first frame update
     public GameManager gManager;
     public CharacterController characterController;
+    private float rotateSpeed = 10;
     void Start()
     {
         gManager = GameManager.GetInstance();
@@ -16,17 +18,6 @@ public class cointrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-
-    void OnTriggerEnter(Collider other)
-    {
-        int team;
-        if (other.gameObject.tag == "Player")
-        {
-            //other.gameObject.GetComponentInParent<PlayerState>().AddPoint(1);
-           // Destroy(gameObject);
-        }
+        transform.rotation *= quaternion.Euler(0, Time.deltaTime * rotateSpeed, 0);
     }
 }
