1e12ec3cb742813bfe6568258c1d1907a3e37727
Hojin Kim <hojiny9923@gmail.com>
2022-04-23 07:08:39 +0900

    대기화면 플랫폼 표시

--- MachineBuildingDestroy/Assets/Scripts/ReadyScene/Slot.cs
+++ MachineBuildingDestroy/Assets/Scripts/ReadyScene/Slot.cs
@@ -11,7 +11,8 @@ public class Slot : MonoBehaviour,IPunObservable
     public GameObject nicknameText;
     public GameObject statusText;
     public GameObject masterButton;
-
+    public GameObject platformText;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -36,17 +37,21 @@ public class Slot : MonoBehaviour,IPunObservable
         // 없으면 변수 초기화
         Nickname = "";
         IsReady = false;
+        Platform = "";
     }
 
     private void SetText()
     {
         nicknameText.GetComponent<Text>().text = Nickname;
         statusText.GetComponent<Text>().text = IsReady ? "ready!" : "";
+        platformText.GetComponent<Text>().text = Platform;
     }
     
     public string Nickname { get; set; } = "";
 
     public bool IsReady { get; set; } = false;
+
+    public string Platform { get; set; } = "";
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
@@ -55,12 +60,14 @@ public class Slot : MonoBehaviour,IPunObservable
         {
             stream.SendNext(Nickname);
             stream.SendNext(IsReady);
+            stream.SendNext(Platform);
         }
         // 리모트 오브젝트이면 읽기 부분이 실행됩니다.
         else
         {
             Nickname = (string) stream.ReceiveNext();
             IsReady = (bool) stream.ReceiveNext();
+            Platform = (string) stream.ReceiveNext();
         }
     }
     
