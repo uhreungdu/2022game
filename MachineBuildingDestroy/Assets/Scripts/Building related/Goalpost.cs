a85ee415ff6c1843a77d17536cf5ddc25222c068
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-04-27 21:35:04 +0900

    씬 다시 저장

--- MachineBuildingDestroy/Assets/Scripts/Building related/Goalpost.cs
+++ MachineBuildingDestroy/Assets/Scripts/Building related/Goalpost.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goalpost : MonoBehaviour
+public class Goalpost : MonoBehaviourPun
 {
+    private GameManager _gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _gameManager = GameManager.GetInstance();
     }
 
     // Update is called once per frame
@@ -16,6 +17,25 @@ public class Goalpost : MonoBehaviour
         
     }
 
+    private void OnEvent()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+        if (box_obj == null && Gmanager.EManager.itembox_Create == true)
+        {
+            if (Item_get == true)
+            {
+                int itemkindLength = System.Enum.GetValues(typeof(item_box_make.item_type)).Length;
+                int rand = Random.Range(1, itemkindLength);
+                rand = 5;
+                photonView.RPC("CreateItem", RpcTarget.MasterClient, rand);
+            }
+        }
+    }
+
+    if (Gmanager.EManager.itembox_Create == false)
+{
+    Item_get = true;
+}
     private void OnTriggerEnter(Collider other)
     {
 
