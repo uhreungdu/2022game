aa40f91a593a4414bb6684e96cb920e85479d72f
2017182026 / youn jang hyurk <34494633+uhreungdu@users.noreply.github.com>
2022-04-30 21:48:46 +0900

    hp이미지바 추가

--- MachineBuildingDestroy/Assets/Scripts/UI_Script/HP_Text_Display.cs
+++ MachineBuildingDestroy/Assets/Scripts/UI_Script/HP_Text_Display.cs
@@ -8,17 +8,23 @@ public class HP_Text_Display : MonoBehaviour
     // Start is called before the first frame update
     public GameManager gManager;
     public Text text_H;
+    public Image Hp_Bar;
     public Text text_Item;
     public item_box_make.item_type item_typ;
+    public PlayerState tempstate;
+    public float Max_hp;
     void Start()
     {
         gManager = GameManager.GetInstance();
+        tempstate = new PlayerState();
+        Max_hp = tempstate.startingHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         text_H.text = "HP: "+gManager.player_stat.health;
+        Hp_Bar.fillAmount = gManager.player_stat.health / 100f;
         item_typ = gManager.player_stat.Item_num;
         switch (item_typ)
         {
