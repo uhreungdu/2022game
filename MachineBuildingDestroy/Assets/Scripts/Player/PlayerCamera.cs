23ba60883541c5cc881e6e39c3a67b9a6e58beb7
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-05-03 02:49:09 +0900

    카메라 끼면 서서히 투명해짐

--- MachineBuildingDestroy/Assets/Scripts/Player/PlayerCamera.cs
+++ MachineBuildingDestroy/Assets/Scripts/Player/PlayerCamera.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using DG.Tweening;
+using ExitGames.Client.Photon.StructWrapping;
 
 public class PlayerCamera : MonoBehaviour
 {
@@ -22,17 +23,32 @@ public class PlayerCamera : MonoBehaviour
     void Update()
     {
         CameraForwardRaycast();
+        HindBuilding();
     }
 
     void HindBuilding()
     {
-        
+        if (_Building != null)
+        {
+            Material material = _Building.GetComponent<Renderer>().material;
+            if (material)
+            {
+                float Opacity;
+                    Opacity = material.GetFloat("_Opacity");
+
+                if (Opacity >= 0.3)
+                {
+                    Opacity -= 2f * Time.deltaTime;
+                    material.SetFloat("_Opacity", Opacity);
+                }
+            }
+        }
     }
     
     void CameraForwardRaycast()
     {
         var ray = new Ray(_Camera.transform.position, _Camera.transform.forward);
-        var maxDistance = 20f;
+        var maxDistance = 10f;
         // 광선 디버그 용도
         Debug.DrawRay(_Camera.transform.position, _Camera.transform.forward * maxDistance, Color.blue);
         RaycastHit _raycastHit;
@@ -41,20 +57,22 @@ public class PlayerCamera : MonoBehaviour
         {
             if (_raycastHit.transform.tag == "Wall" && _raycastHit.transform.gameObject != _Building)
             {
-                MeshRenderer hitMeshRenderer = _raycastHit.transform.GetComponent<MeshRenderer>();
-                if (hitMeshRenderer)
-                {
-                    _Building = _raycastHit.transform.gameObject;
-                    hitMeshRenderer.enabled = false;
-                }
+                // MeshRenderer hitMeshRenderer = _raycastHit.transform.GetComponent<MeshRenderer>();
+                // if (hitMeshRenderer)
+                // {
+                //     hitMeshRenderer.enabled = false;
+                // }
+                _Building = _raycastHit.transform.gameObject;
             }
         }
         else
         {
             if (_Building != null && !Colliding)
             {
-                MeshRenderer beforeCollisionBuilding = _Building.transform.GetComponent<MeshRenderer>();
-                beforeCollisionBuilding.enabled = true;
+                Material material = _Building.GetComponent<Renderer>().material;
+                // MeshRenderer beforeCollisionBuilding = _Building.transform.GetComponent<MeshRenderer>();
+                // beforeCollisionBuilding.enabled = true;
+                material.SetFloat("_Opacity", 1f);
                 _Building = null;
             }
         }
@@ -67,12 +85,13 @@ public class PlayerCamera : MonoBehaviour
             if (other.transform.tag == "Wall" && other.transform.gameObject != _Building)
             {
                 Colliding = true;
-                MeshRenderer hitMeshRenderer = other.transform.GetComponent<MeshRenderer>();
-                if (hitMeshRenderer)
-                {
-                    _Building = other.transform.gameObject;
-                    hitMeshRenderer.enabled = false;
-                }
+                // MeshRenderer hitMeshRenderer = other.transform.GetComponent<MeshRenderer>();
+                // if (hitMeshRenderer)
+                // {
+                //     
+                //     hitMeshRenderer.enabled = false;
+                // }
+                _Building = other.transform.gameObject;
             }
         }
     }
