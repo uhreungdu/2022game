2593641b5eca9bfc59c121e3eb0ce263e0a30966
Hojin Kim <hojiny9923@gmail.com>
2022-05-02 20:59:03 +0900

    on offline 구별 방식 변경

--- WebDBServer/get_player_info.php
+++ WebDBServer/get_player_info.php
@@ -4,12 +4,12 @@ $conn = mysqli_connect("localhost","root","kpu2022project","2022project");
 $id=$_POST['id'];
 
 $result = mysqli_query($conn,"
-SELECT character_name, character_level, customize_1, customize_2
+SELECT character_name, character_level, costume
 FROM `character` 
 WHERE account_id = $id;
 ");
 
 while($row = mysqli_fetch_array($result)){
     echo "character_name:".$row['character_name']."|character_level:".$row['character_level'].
-    "|customize_1:".$row['customize_1']."|customize_2:".$row['customize_2'].";";
+    "|costume:".$row['costume'].";";
 }
--- WebDBServer/login/logout_account.php
+++ WebDBServer/login/logout_account.php
@@ -5,5 +5,5 @@ $id=$_POST['id'];
 
 mysqli_query($conn,
 "UPDATE `character` as C
-SET C.online_status='offline'
+SET C.online=0
 WHERE binary(C.account_id)=$id;");
