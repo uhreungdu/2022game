ed3a4609c7ad5b1dcf6fab9d1d13ef60d8a82cd9
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-05-04 04:06:47 +0900

    게임 끝 UI

--- MachineBuildingDestroy/Assets/Scripts/ReadyScene/GameInfo.cs
+++ MachineBuildingDestroy/Assets/Scripts/ReadyScene/GameInfo.cs
@@ -38,7 +38,8 @@ public class GameInfo : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
             Infomations.gamescore = new int[2];
-        }                       
+            
+        }
         else
         {
             Destroy(gameObject);
