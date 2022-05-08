fac7569f0d8fabcf5de365cb24b3ab66afda4c19
Hojin Kim <hojiny9923@gmail.com>
2022-05-08 20:20:16 +0900

    방 나갔다 들어올 때 컴포넌트 못 찾던 현상 수정

--- MachineBuildingDestroy/Assets/Scripts/ReadyScene/ChangeTeamButton.cs
+++ MachineBuildingDestroy/Assets/Scripts/ReadyScene/ChangeTeamButton.cs
@@ -8,12 +8,12 @@ using UnityEngine.UI;
 // Slot num 0~2 is team1, 3~5 is team2
 public class ChangeTeamButton : MonoBehaviourPun
 {
-    public GameObject Info;
+    private GameObject _info;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _info = GameObject.Find("Myroominfo");
     }
 
     // Update is called once per frame
@@ -24,16 +24,16 @@ public class ChangeTeamButton : MonoBehaviourPun
 
     private void FixedUpdate()
     {
-        transform.GetComponent<Button>().interactable = !Info.GetComponent<MyInRoomInfo>().isReady;
+        transform.GetComponent<Button>().interactable = !_info.GetComponent<MyInRoomInfo>().isReady;
     }
 
     public void OnClick()
     {
-        int mynum = Info.GetComponent<MyInRoomInfo>().mySlotNum;
+        int mynum = _info.GetComponent<MyInRoomInfo>().mySlotNum;
         var slots = GameObject.Find("CharacterSlots").GetComponent<CharacterSlots>();
         var myslot = slots.slots[mynum].GetComponent<Slot>();
         
-        int mySlotNum = Info.GetComponent<MyInRoomInfo>().mySlotNum;
+        int mySlotNum = _info.GetComponent<MyInRoomInfo>().mySlotNum;
         if (mySlotNum % 2 == 0)
         {
             for (int i = 1; i < 6; i+=2)
