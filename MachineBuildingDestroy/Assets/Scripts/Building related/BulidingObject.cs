49d102f64d31dbe052da14efc0a4358ac9518530
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-05-08 22:38:53 +0900

    폭발 버그 수정

--- MachineBuildingDestroy/Assets/Scripts/Building related/BulidingObject.cs
+++ MachineBuildingDestroy/Assets/Scripts/Building related/BulidingObject.cs
@@ -203,18 +203,21 @@ public class BulidingObject : LivingEntity
             //rigidbody.constraints = RigidbodyConstraints.None;
             foreach (Rigidbody child in childRigidbodys)
             {
-                child.constraints = RigidbodyConstraints.None;
-                Vector3 objectPotision = transform.position;
-                objectPotision.y = 1;
-                child.AddExplosionForce(_ExplosionForce, objectPotision, 40f, _ExplosionForce / 2.0f);
-                child.AddExplosionForce(_ExplosionForce, objectPotision, 40f);
+                    child.constraints = RigidbodyConstraints.None;
+                    Vector3 objectPotision = transform.position;
+                    if (child.gameObject != gameObject)
+                    {
+                    objectPotision.y = 1;
+                    child.AddExplosionForce(_ExplosionForce, objectPotision, 40f, _ExplosionForce / 2.0f);
+                    child.AddExplosionForce(_ExplosionForce, objectPotision, 40f);
+                    }
             }
 
             _MeshRenderer.enabled = false;
             _MeshCollider.enabled = false;
             effect_obj = Instantiate(prefeb_effect);
             effect_obj.transform.SetParent(gameObject.transform);
-            effect_obj.transform.Translate(gameObject.transform.position);
+            effect_obj.transform.position = gameObject.transform.position;
             Destroy(GetComponent<PhotonRigidbodyView>());
             Destroy(rigidbody);
         }
