74c2f63be1f497c5b29ac4e097b489eddf5df8d9
Hojin Kim <hojiny9923@gmail.com>
2022-05-10 11:16:01 +0900

    DBServer init

---
+++ CSharpDBServer/CSharpDBServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace DBServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            if (ConnectDB()) { Console.WriteLine("OK"); }
+            else { Console.WriteLine("F"); }
+        }
+
+        private static bool ConnectDB()
+        {
+            string connect = String.Format("Server={0}; Database={1}; Uid={2}; Pwd={3};",
+                "127.0.0.1", "2022project", "root", "2022project");
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connect))
+                {
+                    conn.Open();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
