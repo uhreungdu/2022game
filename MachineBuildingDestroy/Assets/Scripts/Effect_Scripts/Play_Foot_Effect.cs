716458b9d615ce09e269f88d4b43801f95a67eb2
2017182026 / youn jang hyurk <34494633+uhreungdu@users.noreply.github.com>
2022-05-16 01:50:46 +0900

    발판 이펙트 준비중

---
+++ MachineBuildingDestroy/Assets/Scripts/Effect_Scripts/Play_Foot_Effect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_Foot_Effect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject foot_effect;
+    public ParticleSystem foot_VFX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartFooteffect()
+    {
+        
+    }
+}
