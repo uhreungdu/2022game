1e831e4a9be59343d1c6439456bd91b8e6d06b45
2017182026 / youn jang hyurk <34494633+uhreungdu@users.noreply.github.com>
2022-05-16 22:23:47 +0900

    발자국 이펙트 완성

--- MachineBuildingDestroy/Assets/Scripts/Effect_Scripts/Play_Foot_Effect.cs
+++ MachineBuildingDestroy/Assets/Scripts/Effect_Scripts/Play_Foot_Effect.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Play_Foot_Effect : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject foot_effect;
-    public ParticleSystem foot_VFX;
+    public GameObject foot_obj;
     void Start()
     {
         
@@ -20,6 +19,9 @@ public class Play_Foot_Effect : MonoBehaviour
 
     public void StartFooteffect()
     {
-        
+        GameObject create_obj = Instantiate(foot_obj,transform);
+        create_obj.transform.position = gameObject.transform.position + new Vector3(0,0.5f,0);
+        create_obj.transform.parent = null;
+        Destroy(create_obj,1.5f);
     }
 }
