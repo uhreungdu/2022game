e4634ee6c0f8364d95def849e33790ea14573e6d
Hojin Kim <hojiny9923@gmail.com>
2022-07-05 09:27:40 +0900

    버튼 예외 처리
    방 제목 입력 안할시 방 생성 불가
    모든 플레이어 준비 아닐시 게임 시작 불가

--- MachineBuildingDestroy/Assets/Scripts/lobby_test/CreateRoomWindow.cs
+++ MachineBuildingDestroy/Assets/Scripts/lobby_test/CreateRoomWindow.cs
@@ -6,6 +6,7 @@ using System.Text;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
@@ -25,6 +26,7 @@ public class CreateRoomWindow : MonoBehaviour
     public GameObject roomNameField;
     public GameObject playerNumField;
     public GameObject gameModeField;
+    public GameObject okButton;
 
     public GameObject DarkBackground;
 
@@ -43,6 +45,11 @@ public class CreateRoomWindow : MonoBehaviour
         gManager = LobbyManager.GetInstance();
         _account = Account.GetInstance();
         _client = LoginDBConnection.GetInstance().GetClientSocket();
+    }
+
+    private void FixedUpdate()
+    {
+        okButton.GetComponent<Button>().interactable = roomNameField.GetComponent<InputField>().text != "";
     }
 
     public void OnClick(bool val)
