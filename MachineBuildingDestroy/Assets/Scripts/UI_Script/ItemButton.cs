b10f2948803abf2a72a5a245703840dcb9708f5f
Hojin Kim <hojiny9923@gmail.com>
2022-07-22 20:45:15 +0900

    모바일 아이템ui 변화

--- MachineBuildingDestroy/Assets/Scripts/UI_Script/ItemButton.cs
+++ MachineBuildingDestroy/Assets/Scripts/UI_Script/ItemButton.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemButton : MonoBehaviour
 {
+    public GameManager gManager;
     public bool isPressed;
+
+    public Image itemImage;
+    public Sprite[] itemImages;
 
     private void Awake()
     {
+        gManager=GameManager.GetInstance();
         if (Application.platform != RuntimePlatform.Android)
         {
             gameObject.SetActive(false);
@@ -22,5 +28,12 @@ public class ItemButton : MonoBehaviour
     public void ButtonDown()
     {
         isPressed = true;
+    }
+    
+    private void FixedUpdate()
+    {
+        var item_type = gManager.player_stat.Item_num;
+        itemImage.sprite = itemImages[(int)item_type];
+       
     }
 }
