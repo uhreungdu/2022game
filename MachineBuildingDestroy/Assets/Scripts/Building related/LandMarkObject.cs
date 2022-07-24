069d495f0a017926c14f7a1b0432372efe403af5
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-07-25 00:08:34 +0900

    오류 고침
    렌드마크가 리스폰 되던 오류를 수정

--- MachineBuildingDestroy/Assets/Scripts/Building related/LandMarkObject.cs
+++ MachineBuildingDestroy/Assets/Scripts/Building related/LandMarkObject.cs
@@ -24,6 +24,7 @@ public class LandMarkObject : BulidingObject
         _Colliders = GetComponents<Collider>();
         _rigidbody = GetComponent<Rigidbody>();
         originTransform = transform.position;
+        ItIsLandMark = true;
     }
     void Update()
     {
