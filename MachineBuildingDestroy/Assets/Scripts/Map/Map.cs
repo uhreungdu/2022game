5d7a847ea38a3cf8ed1e3c578b2389e376fcdc17
Hojin Kim <hojiny9923@gmail.com>
2022-07-28 14:41:55 +0900

    맵 리스트 만들기

--- MachineBuildingDestroy/Assets/Scripts/Map/Map.cs
+++ MachineBuildingDestroy/Assets/Scripts/Map/Map.cs
@@ -167,24 +167,40 @@ public class Map : MonoBehaviour
     
     public static List<string> LoadNameFile()
     {
+        BetterStreamingAssets.Initialize();
         String FolderName = Application.streamingAssetsPath + "/" + "Map";
         System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(FolderName);
         List<string> NameList = new List<string>();
-        if (di.Exists)
+
+        if (RuntimePlatform.Android == Application.platform)
         {
-            foreach (System.IO.FileInfo File in di.GetFiles())
+            //loaded to Map/example.json
+            string[] paths = BetterStreamingAssets.GetFiles("Map", "*.json");
+            foreach (var filepath in paths)
             {
-                string extension = ".json";
-                if (File.Extension.ToLower().CompareTo(extension) == 0)
+                string filename = filepath.Substring(4, filepath.Length - 4 - 5);
+                NameList.Add(filename);
+            }
+        }
+        else
+        {
+            if (di.Exists)
+            {
+                foreach (System.IO.FileInfo File in di.GetFiles())
                 {
-                    String FileNameOnly = File.Name.Substring(0, File.Name.Length - extension.Length);
-                    String FullFileName = File.FullName;
-                    String loadPath = FolderName;
-                    NameList.Add(FileNameOnly);
-                    print(FullFileName + " " + FileNameOnly);
+                    string extension = ".json";
+                    if (File.Extension.ToLower().CompareTo(extension) == 0)
+                    {
+                        String FileNameOnly = File.Name.Substring(0, File.Name.Length - extension.Length);
+                        String FullFileName = File.FullName;
+                        String loadPath = FolderName;
+                        NameList.Add(FileNameOnly);
+                        print(FullFileName + " " + FileNameOnly);
+                    }
                 }
             }
         }
+
         return NameList;
     }
     public void LoadMapFile(string loadPath, string fileName)
