48ac03d17d28c8ecb178f0b4a88eeebb6ca91ecb
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-07-30 22:53:53 +0900

    PlayerInteract.cs수정
    접시에 점수를 넣었을 때 갱신이 안되던 문제를 수정

--- MachineBuildingDestroy/Assets/Scripts/Player/PlayerInteract.cs
+++ MachineBuildingDestroy/Assets/Scripts/Player/PlayerInteract.cs
@@ -82,7 +82,7 @@ public class PlayerInteract : MonoBehaviourPun
                             photonView.RPC("GetPointCount", RpcTarget.AllViaServer, Slotnum, 0);
                             photonView.RPC("NetWorkResetPoint", RpcTarget.AllViaServer);
                         }
-                        photonView.RPC("SetOnHeadCoinNum", RpcTarget.AllViaServer, 0);
+                        photonView.RPC("SetOnHeadCoinNum", RpcTarget.AllViaServer, 0.ToString());
                     }
                     if (photonView.IsMine)
                         _Progressbar.gameObject.SetActive(false);
