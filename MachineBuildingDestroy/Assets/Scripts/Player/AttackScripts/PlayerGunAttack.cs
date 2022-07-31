374c7dfcd52ceb8400cc99cbf4971f27ddccbe2d
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2022-08-01 01:08:44 +0900

    PlayergunAttack 수정
    rpc로 갈아엎음

--- MachineBuildingDestroy/Assets/Scripts/Player/AttackScripts/PlayerGunAttack.cs
+++ MachineBuildingDestroy/Assets/Scripts/Player/AttackScripts/PlayerGunAttack.cs
@@ -88,15 +88,20 @@ public class PlayerGunAttack : PlayerAttack
         GameObject BulletgameObject = PhotonNetwork.Instantiate(_gunBulletGameObject.name, GunObject.transform.position + -GunObject.transform.right,
             _gunBulletGameObject.transform.rotation);
         GunBullet BulletgameObjectGunBullet = BulletgameObject.GetComponent<GunBullet>();
-        BulletgameObjectGunBullet.ShootTeam = _playerState.team;
-        BulletgameObjectGunBullet.ShootPosition = GunObject.transform.position;
-        Vector3 returnForwardVector = transform.forward;
-        returnForwardVector = Quaternion.Euler(Random.Range(0.0f, 5.0f), Random.Range(0.0f, 5.0f), 0) * transform.forward;
-        BulletgameObjectGunBullet.ForwardVector3 = returnForwardVector;
-        BulletgameObjectGunBullet._playerGunAttack = this;
-        BulletgameObjectGunBullet.ShootNickName = _playerState.NickName;
+        photonView.RPC("BulletInfomation", RpcTarget.AllViaServer, BulletgameObjectGunBullet);
         _Gun.Durability -= 1;
         yield return new WaitForSeconds(0.025f);
         isDelay = false;
+    }
+
+    [PunRPC]
+    void BulletInfomation(GunBullet gunBullet)
+    {
+        gunBullet.ShootPosition = GunObject.transform.position;
+        Vector3 returnForwardVector = transform.forward;
+        returnForwardVector = Quaternion.Euler(Random.Range(0.0f, 5.0f), Random.Range(0.0f, 5.0f), 0) * transform.forward;
+        gunBullet.ForwardVector3 = returnForwardVector;
+        gunBullet._playerGunAttack = this;
+        gunBullet.ShootNickName = _playerState.NickName;
     }
 }
